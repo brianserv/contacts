d69bb16bb9e326e3ecf38e4b7406c7311b3e76a6
jimingmin <jimingmin@gmail.com>
2015-04-26 00:00:13 +0800

    增加关注消息的注册

--- regist_message.h
+++ regist_message.h
@@ -14,12 +14,14 @@
 #include "../include/contacts_msg.h"
 #include "logic/setuserinfo_handler.h"
 #include "logic/getuserinfo_handler.h"
+#include "logic/followuser_handler.h"
 
 using namespace FRAME;
 
 MSGMAP_BEGIN(msgmap)
 ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_SETUSERINFO_REQ, ControlHead, MsgHeadCS, CSetUserInfoReq, CSetUserInfoHandler, CSetUserInfoHandler::SetUserInfo);
 ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_GETUSERINFO_REQ, ControlHead, MsgHeadCS, CGetUserInfoReq, CGetUserInfoHandler, CGetUserInfoHandler::GetUserInfo);
+ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_FOLLOWUSER_REQ, ControlHead, MsgHeadCS, CFollowUserReq, CFollowUserHandler, CFollowUserHandler::FollowUser);
 MSGMAP_END(msgmap)
 
 #endif /* REGIST_MESSAGE_H_ */
